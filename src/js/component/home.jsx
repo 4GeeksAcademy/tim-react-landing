102fab2502ea8f500cd2d41f37c5a7b199ab9427
Tim <timgibson49@gmail.com>
2024-03-08 23:28:02 +0000

    navbar outline

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import MyNavbar from './navbar'
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
+			<MyNavbar />
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
 			<p>
 				<img src={rigoImage} />
@@ -14,11 +16,6 @@ const Home = () => {
 			<a href="#" className="btn btn-success">
 				If you see this green button... bootstrap is working...
 			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
 		</div>
 	);
 };
