781dd3f59e64d4028cc2e870b9048355c7155742
Michel Rivero <157402536+michelriv@users.noreply.github.com>
2024-03-09 00:35:42 +0000

    jumbotron

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,18 +3,20 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 import MyNavbar from './navbar'
+import Jumbotron from "./jumbotron";
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
 			<MyNavbar />
-			
+			<Jumbotron />
 			
 		</div>
-		
+
 	);
 };
+
 
 
 
